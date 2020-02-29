6398097a872f51194a945fbf4b44640e7381f9d0
Guidry <jh_guidry@outlook.com>
2020-02-29 01:35:50 -0600

    styles for navigation menu added with js for toggling

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -9054,6 +9054,10 @@
   }));
   });
 
+  const mobileToggle = document.querySelector(".nav-toggle");
+  const headerNav = document.querySelector(".nav-menu");
+  const navList = document.querySelector(".nav-list");
+
   const banner = tns({
     container: '.slider',
     mode: "gallery",
@@ -9083,5 +9087,24 @@
     field: document.getElementById('datepicker') ,
     format: 'MMM D YYYY',
   });
+
+  function toggleNav() {
+    headerNav.classList.toggle("nav-active");
+    mobileToggle.classList.toggle("toggle-height");
+    document.querySelector("body").classList.toggle("no-scroll");
+    mobileToggle.children[1].classList.toggle("turn-cross-1");
+    mobileToggle.children[2].classList.toggle("invisible");
+    mobileToggle.children[3].classList.toggle("turn-cross-3");
+    if(mobileToggle.getAttribute("aria-expanded") === "false"
+    && navList.getAttribute("aria-hidden") === "true") {
+        mobileToggle.setAttribute("aria-expanded", "true");
+        navList.setAttribute("aria-hidden", "false");
+    } else {
+        mobileToggle.setAttribute("aria-expanded", "false");
+        navList.setAttribute("aria-hidden", "true");
+    }
+  }
+
+  mobileToggle.addEventListener("click", toggleNav);
 
 })));
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { tns } from "../node_modules/tiny-slider/src/tiny-slider.js";
 import Pikaday from "pikaday";
+const mobileToggle = document.querySelector(".nav-toggle");
+const headerNav = document.querySelector(".nav-menu");
+const navList = document.querySelector(".nav-list");
 
 const banner = tns({
   container: '.slider',
@@ -30,3 +33,22 @@ var picker = new Pikaday({
   field: document.getElementById('datepicker') ,
   format: 'MMM D YYYY',
 });
+
+function toggleNav() {
+  headerNav.classList.toggle("nav-active");
+  mobileToggle.classList.toggle("toggle-height");
+  document.querySelector("body").classList.toggle("no-scroll");
+  mobileToggle.children[1].classList.toggle("turn-cross-1");
+  mobileToggle.children[2].classList.toggle("invisible");
+  mobileToggle.children[3].classList.toggle("turn-cross-3");
+  if(mobileToggle.getAttribute("aria-expanded") === "false"
+  && navList.getAttribute("aria-hidden") === "true") {
+      mobileToggle.setAttribute("aria-expanded", "true");
+      navList.setAttribute("aria-hidden", "false");
+  } else {
+      mobileToggle.setAttribute("aria-expanded", "false");
+      navList.setAttribute("aria-hidden", "true");
+  }
+}
+
+mobileToggle.addEventListener("click", toggleNav);
