56ed3e12b4ff138bed954f736ef8bdeefc139f8f
Guidry <jh_guidry@zoho.com>
2024-03-02 03:37:22 -0600

    slider removed and adjustments made

--- src/main.js
+++ src/main.js
@@ -1,60 +1,7 @@
-import { tns } from "./../node_modules/tiny-slider/src/tiny-slider";
 import Pikaday from "pikaday";
 const mobileToggle = document.querySelector(".nav-toggle");
 const headerNav = document.querySelector(".nav-menu");
 const navList = document.querySelector(".nav-list");
-
-
-
-if(document.querySelector(".slider")) {
-  const bannerSlide = tns({
-    container: '.slider',
-    mode: "gallery",
-    axis: "vertical",
-    items: 1,
-    slideBy: 'page',
-    autoplay: true,
-    controls: false,
-    autoplayButtonOutput: false,
-    navPosition: "bottom"
-  });
-
-  function changeBgImage () {
-    let slides = Array.from(document.querySelector(".slider").children);
-    let banner = document.querySelector(".banner");
-    const bgArray = [
-      'url(images/img_bg_1.jpg)',
-      'url(images/img_bg_2.jpg)',
-      'url(images/img_bg_3.jpg)',
-      'url(images/img_bg_4.jpg)'
-    ];
-    slides.forEach(slide => {
-      let active = slide.classList.contains("tns-slide-active");
-      if (active === true) {
-        let id;
-        id = slide === slides[slides.length - 1] ? 0 : parseInt(slide.id.charAt(slide.id.length -1));
-        let bgImg = bgArray[id];
-        banner.style.backgroundImage = bgImg;
-      } 
-    });
-  }
-
-  bannerSlide.events.on("indexChanged", changeBgImage);
-}
-
-if (document.querySelector(".client-slider")){
-  const clientSlide = tns({
-    container: '.client-slider',
-    mode: "gallery",
-    axis: "vertical",
-    items: 1,
-    slideBy: 'page',
-    autoplay: true,
-    controls: false,
-    autoplayButtonOutput: false,
-    navPosition: "bottom"
-  });
-}
 
 function toggleNav() {
   headerNav.classList.toggle("nav-active");
@@ -63,13 +10,15 @@ function toggleNav() {
   mobileToggle.children[1].classList.toggle("turn-cross-1");
   mobileToggle.children[2].classList.toggle("invisible");
   mobileToggle.children[3].classList.toggle("turn-cross-3");
-  if(mobileToggle.getAttribute("aria-expanded") === "false"
-  && navList.getAttribute("aria-hidden") === "true") {
-      mobileToggle.setAttribute("aria-expanded", "true");
-      navList.setAttribute("aria-hidden", "false");
+  if (
+    mobileToggle.getAttribute("aria-expanded") === "false" &&
+    navList.getAttribute("aria-hidden") === "true"
+  ) {
+    mobileToggle.setAttribute("aria-expanded", "true");
+    navList.setAttribute("aria-hidden", "false");
   } else {
-      mobileToggle.setAttribute("aria-expanded", "false");
-      navList.setAttribute("aria-hidden", "true");
+    mobileToggle.setAttribute("aria-expanded", "false");
+    navList.setAttribute("aria-hidden", "true");
   }
 }
 
@@ -82,10 +31,10 @@ if (document.querySelector(".tab-links")) {
     chosenTab.focus();
     chosenTab.setAttribute("aria-selected", "true");
     chosenTab.classList.toggle("active");
-    oldTab.setAttribute("aria-selected", 'false');
+    oldTab.setAttribute("aria-selected", "false");
     oldTab.classList.toggle("active");
-    let index = Array.prototype.indexOf.call(tabs,chosenTab);
-    let oldIndex = Array.prototype.indexOf.call(tabs,oldTab);
+    let index = Array.prototype.indexOf.call(tabs, chosenTab);
+    let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
     panels[oldIndex].classList.toggle("active");
     panels[oldIndex].setAttribute("aria-hidden", "true");
     panels[index].classList.toggle("active");
@@ -93,38 +42,46 @@ if (document.querySelector(".tab-links")) {
   }
 
   // add click and keyboard event listeners to tabs
-  tabs.forEach(function(tab, i) {
-    tab.addEventListener("click", function(event) {
-        event.preventDefault();
-        const currentlySelectedTab = document.querySelector('[aria-selected="true"]');
-        const newlySelectedTab = event.currentTarget;
-        if (currentlySelectedTab !== newlySelectedTab) {
-            switchTab(currentlySelectedTab, newlySelectedTab);
-        }
+  tabs.forEach(function (tab, i) {
+    tab.addEventListener("click", function (event) {
+      event.preventDefault();
+      const currentlySelectedTab = document.querySelector(
+        '[aria-selected="true"]'
+      );
+      const newlySelectedTab = event.currentTarget;
+      if (currentlySelectedTab !== newlySelectedTab) {
+        switchTab(currentlySelectedTab, newlySelectedTab);
+      }
     });
-    
-    tab.addEventListener('keydown', e => {  
-        let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-        let dir = e.which === 37 ? index - 1 
-                : e.which === 39 ? index + 1 
-                : e.which === 40 ? 'down' : null;
 
-        if (dir !== null) {
-            e.preventDefault();
-            dir === 'down' ? panels[i].focus() 
-                : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) 
-                : void 0;
-        }
+    tab.addEventListener("keydown", (e) => {
+      let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+      let dir =
+        e.which === 37
+          ? index - 1
+          : e.which === 39
+          ? index + 1
+          : e.which === 40
+          ? "down"
+          : null;
+
+      if (dir !== null) {
+        e.preventDefault();
+        dir === "down"
+          ? panels[i].focus()
+          : tabs[dir]
+          ? switchTab(e.currentTarget, tabs[dir])
+          : void 0;
+      }
     });
   });
 }
 
-if (document.getElementById('datepicker')) {
-  var picker = new Pikaday({ 
-    field: document.getElementById('datepicker') ,
-    format: 'MMM D YYYY',
+if (document.getElementById("datepicker")) {
+  var picker = new Pikaday({
+    field: document.getElementById("datepicker"),
+    format: "MMM D YYYY",
   });
 }
-
 
 mobileToggle.addEventListener("click", toggleNav);
